015384e6beb97ea5929fe018c8c873c0e95d9952
Char1esH <charleshuibang@gmail.com>
2021-03-30 10:00:17 +0800

    adjusted repo structure

--- test_hp.py
+++ test_hp.py
@@ -7,11 +7,12 @@ from SimpleHPNet import SimpleHPNet
 from models.utils.visualization import joints_dict, draw_points_and_skeleton
 
 #model = SimpleHPNet(48, 17, "weights/hrnet/pose_hrnet_w48_384x288.pth", model_name='hrnet')
-model = SimpleHPNet(101, 17, "weights/resnet/pose_resnet_101_384x288.pth", model_name='poseresnet')
-#model = SimpleHPNet(8, 16, 'weights/hg/bearpaw_hg8.pth', model_name='hg', resolution=(256, 256))
+#model = SimpleHPNet(101, 17, "weights/resnet/pose_resnet_101_384x288.pth", model_name='poseresnet')
+model = SimpleHPNet(8, 16, 'weights/hg/bearpaw_hg8.pth', model_name='hg', resolution=(256, 256))
 
-
-image = cv2.imread("./test_img/test_img002.jpg", cv2.IMREAD_COLOR)
+imagesrc = './test_img/test_img004.jpg'
+imagedst = './result_img/result_hg_mpii004.jpg'
+image = cv2.imread(imagesrc, cv2.IMREAD_COLOR)
 print(image.shape)
 
 pts = model.predict(image)
@@ -19,14 +20,14 @@ pts = pts[0]
 print(pts)
 print(pts.shape)
 
-# new_image = draw_points_and_skeleton(image, pts, joints_dict()['mpii']['skeleton'], 
-#                                      person_index = 0, confidence_threshold = 0.3, 
-#                                      points_color_palette='gist_rainbow', skeleton_color_palette='jet', 
-#                                      points_palette_samples=8)
-
-new_image = draw_points_and_skeleton(image, pts, joints_dict()['coco']['skeleton'], 
+new_image = draw_points_and_skeleton(image, pts, joints_dict()['mpii']['skeleton'], 
                                      person_index = 0, confidence_threshold = 0.3, 
                                      points_color_palette='gist_rainbow', skeleton_color_palette='jet', 
                                      points_palette_samples=8)
 
-cv2.imwrite('result_img/result_resnet_coco.jpg', new_image)
+# new_image = draw_points_and_skeleton(image, pts, joints_dict()['coco']['skeleton'], 
+#                                      person_index = 0, confidence_threshold = 0.3, 
+#                                      points_color_palette='gist_rainbow', skeleton_color_palette='jet', 
+#                                      points_palette_samples=8)
+
+cv2.imwrite(imagedst, new_image)
